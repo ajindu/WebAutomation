de600db62e71f225e94196025e3e1082b495ce21
Sang Aji <aji.indu06@gmail.com>
2024-11-21 13:46:56 +0700

    Demo API Automation

---
+++ src/test/java/pages/ApiPage.java
@@ -0,0 +1,111 @@
+package pages;
+
+import helper.Endpoint;
+import helper.Utility;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.assertj.core.data.Index;
+
+import java.io.File;
+import java.util.List;
+import java.util.Objects;
+
+import static helper.Models.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApiPage {
+
+    String setUrl, global_id;
+    Response res;
+
+    public void prepareURL(){
+        System.out.println("Prepare URL");
+    }
+    public void prepareUrlFor(String url){
+        switch (url){
+            case "GET_LIST_USERS":
+                setUrl = Endpoint.GET_LIST_USERS;
+                break;
+            case "CREATE_NEW_USERS":
+                setUrl = Endpoint.CREATE_NEW_USERS;
+                break;
+            case "DELETE_USERS":
+                setUrl = Endpoint.DELETE_USERS;
+                break;
+            default:
+                System.out.println("input right url");
+        }
+    }
+    public void hitApiGetListUsers(){
+        res = getListUsers(setUrl);
+        //System.out.println(res.getBody().asString());
+    }
+    public void validaionStatusCodeIsEquals(int status_code){
+        assertThat(res.statusCode()).isEqualTo(status_code);
+    }
+    public void validationResponseBodyGetListUsers(){
+        List<Object> id = res.jsonPath().getList("id");
+        List<Object> name = res.jsonPath().getList("name");
+        List<Object> email = res.jsonPath().getList("email");
+        List<Object> gender = res.jsonPath().getList("gender");
+        List<Object> status = res.jsonPath().getList("status");
+
+        assertThat(id.get(0)).isNotNull();
+        assertThat(name.get(0)).isNotNull();
+        assertThat(email.get(0)).isNotNull();
+        assertThat(gender.get(0)).isIn("female", "male");
+        assertThat(status.get(0)).isIn("active", "inactive");
+
+    }
+    public void validationResponseJsonWithJSONSchema(String filename){
+        File JSONFile = Utility.getJSONSchemaFile(filename);
+        res.then().assertThat().body(JsonSchemaValidator.matchesJsonSchema(JSONFile));
+    }
+
+    public void hitApiPostCreateUser(){
+        res = postCreateUser(setUrl);
+        System.out.println(res.getBody().asString());
+    }
+
+    public void validationResponseBodyCreateUser(){
+        JsonPath jsonPathEvaluator = res.jsonPath();
+        Integer id = jsonPathEvaluator.get("id");
+        String name = jsonPathEvaluator.get("name");
+        String email = jsonPathEvaluator.get("email");
+        String gender = jsonPathEvaluator.get("gender");
+        String status = jsonPathEvaluator.get("status");
+
+        assertThat(id).isNotNull();
+        assertThat(name).isNotNull();
+        assertThat(email).isNotNull();
+        assertThat(gender).isIn("female", "male");
+        assertThat(status).isIn("active", "inactive");
+
+        global_id = Integer.toString(id);
+    }
+
+    public void hitApiDeleteUser(){
+        res = deleteUser(setUrl, global_id);
+    }
+
+    public void hitApiUpdateUser(){
+        res = updateUser(setUrl, global_id);
+    }
+
+    public void validationResponseBodyUpdateUser(){
+        JsonPath jsonPathEvaluator = res.jsonPath();
+        Integer id = jsonPathEvaluator.get("id");
+        String name = jsonPathEvaluator.get("name");
+        String email = jsonPathEvaluator.get("email");
+        String gender = jsonPathEvaluator.get("gender");
+        String status = jsonPathEvaluator.get("status");
+
+        assertThat(id).isNotNull();
+        assertThat(name).isNotNull();
+        assertThat(email).isNotNull();
+        assertThat(gender).isIn("female", "male");
+        assertThat(status).isIn("active", "inactive");
+
+    }
+}
